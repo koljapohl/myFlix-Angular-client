fa131d6fd8f53a3c314b184cbf8eb5e6249edbab
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-09-07 12:54:11 +0200

    [ADD] proj setup + fetch data service

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
+To open the application in a new tab in the browser directly, run `ng serve --open`
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
@@ -25,3 +27,63 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Project description
+
+This project's objective is to build a chat app for mobile devices using React Native. The app will
+provide users with a chat interface and options to share images and their location.
+
+## Key features
+
+● A page where users can enter their name and choose a background color for the chat screen before joining the chat.
+● A page displaying the conversation, as well as an input field and submit button.
+● The chat must provide users with two additional communication features: sending images and location data.
+● Data gets stored online and offline.
+
+## Dependencies
+
++ React
++ ReactDOM
++ React-Scripts
++ React-Native
++ React-Navigation
++ React-native-gifted-chat
++ React-native-asyncStorage
++ Firebase
++ Expo
++ Expo-image-picker
++ Expo-location
+
+## Tools used
+
++ AngularJS
+
++ Expo
++ Firebase Authentication (anonymously)
++ Firestore DB
++ Firebase Storage
++ Android Studio
+
+## API
+
+## getting started
+
+Requirements:
+
++ Node.js
++ Angular CLI
+
+`npm install -g @angular/cli`
+
+## Setup
+
++ clone repo to your hard drive
++ install dependencies
+
+`npm install`
+
++ within root folder start of dev server by
+
+`npm run start` OR `expo start`
+
++ run chatApp with an emulator/simulator of your choice or via the ExpoGo application on your physical device
---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,158 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+//Declaring the api url that will provide data for the client app
+const apiUrl = 'https://myflix-kp.herokuapp.com/';
+//get token from localStorage
+const token = localStorage.getItem('token');
+//get username from localStorage
+const username = localStorage.getItem('username');
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchApiDataService {
+//injecting the HttpClient module to the constructor params
+  // will provide HttpClient to the entire class, making it available via this.http
+  constructor (private http: HttpClient) { }
+
+  //User registration (public service)
+  public userRegistration (userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'users', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  //User login (public service)
+  public userLogin (userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'login', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+ //get all movies
+  getAllMovies (): Observable<any> {
+    return this.http.get(apiUrl + 'movies', {
+      headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  //get a single movie by movieId
+  getMovie (mId: string): Observable<any> {
+    return this.http.get(apiUrl + `movies/test/${mId}`, {
+      headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  //get a director by name
+  getDirector (name: string): Observable<any> {
+    return this.http.get(apiUrl + `movies/directors/${name}`, {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  //get a genre by title
+  getGenre (title: string): Observable<any> {
+    return this.http.get(apiUrl + `/movies/genres/${title}`, {
+      headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  //get a user by username
+  getUser (): Observable<any> {
+    return this.http.get(apiUrl + `users/${username}`, {
+      headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  //add a movie to user's favorites
+  addMovieToFav (mId: string): Observable<any> {
+    return this.http.post(apiUrl + `/users/${username}/movies/${mId}`, {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError));
+  }
+
+  //delete a movie from user's favorites
+  delMovieFromFav (mId: string): Observable<any> {
+    return this.http.delete(apiUrl + `/users/${username}/movies/${mId}`, {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError));
+  }
+
+  //edit user's credentials
+  editUser (userDetails: any): Observable<any> {
+    console.log(userDetails);
+      return this.http.put(apiUrl + `/users/${username}`, userDetails, {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + token,
+        })
+      }).pipe(
+        map(this.extractResponseData),
+        catchError(this.handleError));
+  }
+
+  //delete a user's account
+  delUser (): Observable<any> {
+    return this.http.delete(apiUrl + `/users/${username}`, {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError));
+  }
+
+  //non-typed response extraction
+  private extractResponseData (res: Response | Object): any {
+    const body = res;
+    return body || {};
+  }
+
+  private handleError (error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred: ' + error.error.message);
+    } else {
+      console.error(
+        `Error status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
