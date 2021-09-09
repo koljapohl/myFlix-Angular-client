f2760fc7df7938f944b1ea87e0d78e188e43e368
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-09-09 12:43:01 +0200

    [ADD] app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { UserRegistrationFormComponent } from 'src/app/user-registration-form/user-registration-form.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor (public dialog: MatDialog) { }
+  // opens the dialog for registering after the according button has been clicked
+  openUserRegistrationDialog (): void {
+    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+  }
 }
